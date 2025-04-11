7655d7d5d95f715f6364f608b6fcabb871e07bad
Colby <maquiret@gmail.com>
2025-04-11 12:53:06 -0500

    wokred on some before work

--- Game/gpt3.js
+++ Game/gpt3.js
@@ -4,7 +4,21 @@ console.log("Hello World!");
 
 // 2.Create a button to gain XP
 //  #Each click gives +10 XP.
+let counter = 0;
+let maxLevel = 10;
+let expDisplay = document.getElementById("exp");
+let experience = document.getElementById("expBtn");
 
+function gain(expDisplay, maxLevel) {
+    experience.addEventListener("click", () => {
+        if (counter < maxLevel) {
+            counter++;
+            exp.textContent = counter;
+        } else {
+            alert("Level Up");
+        }
+    })
+}
 // 3.Level Up Mechanic
 //  #When XP hits 100:
 //  #Increase the level by 1
