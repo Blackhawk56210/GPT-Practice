cb9320500e67fc1177baa74ed2312b9c0ec71655
Colby <maquiret@gmail.com>
2025-04-28 18:54:57 -0500

    added a small project

--- task-manager-app/script.js
+++ task-manager-app/script.js
@@ -20,18 +20,53 @@ Each task should be an object with taskName and status properties.
 Use .push(), .splice(), and .split() as needed.
 */ 
 
-
-
 let taskList = [
     { taskName: "laundry", status: "incomplete" },
     { taskName: "dishes", status: "incomplete" },
-  ];
+];
   
 // Example operations:
 //1. Add a new task: `"Homework"`
 //2. Remove the task `"dishes"`
 //3. Mark `"laundry"` as completed
-  
+function addTask() {
+  let taskInput = document.getElementById("inputField");
+  let task = JSON.parse(localStorage.getItem("tasks")) || [];
+  task.push(taskInput.value);
+  localStorage.setItem("tasks", JSON.stringify(task));
+  displayTask();
+  taskInput.value = "";
+}
+
+function displayTask() {
+  let taskList = document.getElementById("taskList");
+  taskList.innerHTML = "";
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.forEach((tasks, index) => {
+    const li = document.createElement("li");
+    li.innerHTML = 
+    `${tasks} <button onclick="removeTask(${index})">remove</button>
+    <input type="checkbox" id="status" value="complete" name="completeTask"></input>`;
+    taskList.appendChild(li);
+  })
+}
+
+// function completeTask() {
+//   let taskList = document.getElementById("taskList");
+//   taskList.innerHTML = "";
+//   let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+//   tasks.forEach((tasks, index) => {
+//     const li = 
+//   })
+// }
+
+function removeTask(index) {
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.splice(index, 1);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  displayTask();
+}
+
 /*
 Expected Output:
 After adding "Homework", the task list should have 3 tasks.
