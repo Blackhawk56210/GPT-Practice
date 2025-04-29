0207f14aa4427a5e1285830fa3d588cd9d8ed7a8
Colby <maquiret@gmail.com>
2025-04-28 20:19:19 -0500

    toggle status

--- task-manager-app/script.js
+++ task-manager-app/script.js
@@ -1,16 +1,18 @@
 console.log("I created the perfect system");
+toggleStatus();
+
 /*
 You are building a simple task manager. The user will add tasks, remove them, and mark tasks as completed.
 
 You need to implement the following functionality:
 
-Add a new task to the task list.
+Add a new task to the task list. DONE
 
-Remove a task by task name.
+Remove a task by task name.  ??
 
-Mark a task as completed (change the task’s status to "completed").
+Mark a task as completed (change the task’s status to "completed"). DONE?
 
-Display the task list (including whether each task is completed or not).
+Display the task list (including whether each task is completed or not). Done?
 
 Steps to complete:
 Use an array to store tasks.
@@ -24,15 +26,11 @@ let taskList = [
     { taskName: "laundry", status: "incomplete" },
     { taskName: "dishes", status: "incomplete" },
 ];
-  
-// Example operations:
-//1. Add a new task: `"Homework"`
-//2. Remove the task `"dishes"`
-//3. Mark `"laundry"` as completed
+
 function addTask() {
   let taskInput = document.getElementById("inputField");
   let task = JSON.parse(localStorage.getItem("tasks")) || [];
-  task.push(taskInput.value);
+  task.push({ taskName: "homework", status: "incomplete" });
   localStorage.setItem("tasks", JSON.stringify(task));
   displayTask();
   taskInput.value = "";
@@ -42,23 +40,20 @@ function displayTask() {
   let taskList = document.getElementById("taskList");
   taskList.innerHTML = "";
   let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-  tasks.forEach((tasks, index) => {
+  tasks.forEach((task, index) => {
     const li = document.createElement("li");
     li.innerHTML = 
-    `${tasks} <button onclick="removeTask(${index})">remove</button>
-    <input type="checkbox" id="status" value="complete" name="completeTask"></input>`;
+    `${task.taskName} 
+    <button onclick="removeTask(${index})">remove</button>
+    <input type="checkbox" ${task.status === "completed" ? "checked" : ""} onclick="toggleStatus(${index})">`;
     taskList.appendChild(li);
   })
 }
 
-// function completeTask() {
-//   let taskList = document.getElementById("taskList");
-//   taskList.innerHTML = "";
-//   let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-//   tasks.forEach((tasks, index) => {
-//     const li = 
-//   })
-// }
+function toggleStatus(index) {
+  let task = JSON.parse(localStorage.getItem("tasks")) || [];
+  console.log(task);
+}
 
 function removeTask(index) {
   let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
