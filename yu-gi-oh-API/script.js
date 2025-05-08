789fa08aab290835ca3334bd83004a13753c923d
Colby <maquiret@gmail.com>
2025-05-08 11:42:39 -0500

    fin yu-gi-oh api

--- yu-gi-oh-API/script.js
+++ yu-gi-oh-API/script.js
@@ -1,6 +1,7 @@
 const input = document.getElementById("cardName");
 const button = document.getElementById("searchBtn");
 const cardDisplay = document.getElementById("cardInfo");
+const themeToggle = document.getElementById("themeToggle");
 
 themeToggle.addEventListener("change", toggleTheme);
 
@@ -15,18 +16,32 @@ button.addEventListener("click", () => {
     .then(res => res.json())
     .then( data => {
         const card = data.data[0];
+        let cardType = card.type;
+        let badgeText = "";
+        let badgeClass = "";
+        if (cardType.includes("Spell")) {
+           badgeText = "🧙 Spell";
+           badgeClass = "badge-spell";
+        } else if (cardType.includes("Trap")) {
+            badgeText = "💣 Trap";
+            badgeClass = "badge-trap";
+        } else {
+            badgeText = "👹 Monster";
+            badgeClass = "badge-monster"
+        }
         const name = card.name;
         const type = card.type;
         const img = card.card_images[0].image_url;
         const desc = card.desc;
         const race = card.race;
-        const atk = card.atk;
-        const def = card.def;
-        const level = card.level;
+        const atk = card.atk !== undefined ? `Atk ${card.atk}` : "";
+        const def = card.def !== undefined ? `Def ${card.def}` : "";
+        const level = card.level !== undefined ? `Level ${card.level}` : "";
         cardDisplay.innerHTML = `
         <h1>${name}</h1>
-        <p>Level: ${level}</p>
-        <img src="${img}"/>
+        <p class="badge ${badgeClass}">${badgeText}</p>
+        <p>${level}</p>
+        <img src="${img}" alt="${name}"/>
         <p>Type: ${race}/${type}</p>
         <p>Atk: ${atk} / Def: ${def}</p>
         <p>${desc}</p>
@@ -39,7 +54,6 @@ button.addEventListener("click", () => {
     console.log(`Fetching URL: https://db.ygoprodeck.com/api/v7/cardinfo.php?name=${encodeURIComponent(cardName)}`);
     console.log(cardName);
 })
-
 
 function toggleTheme() {
     document.body.classList.toggle("dark", themeToggle.checked);
