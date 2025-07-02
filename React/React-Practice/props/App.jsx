b05036704e9974ec217f1a83834273988b0d2190
Colby <maquiret@gmail.com>
2025-07-01 22:01:29 -0500

    changed folder structure

---
+++ React/React-Practice/props/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='text-white p-8'>
+      <h1 className='text-2xl font-bold'>Props Practice</h1>
+    </div>
+  )
+}
+
+export default App
