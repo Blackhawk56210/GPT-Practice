a0160e354bdc5a971ab7dde1b04925eb3f65d8f2
Colby <maquiret@gmail.com>
2025-07-02 19:38:40 -0500

    did some conditionals

--- React/React-Practice/conditionals/App.jsx
+++ React/React-Practice/conditionals/App.jsx
@@ -1,15 +1,37 @@
 import React, { useState } from "react";
 
 function App() {
-  const [showMessage, setShowMessage] = useState(true);
-
+  const [showSection, setShowSection] = useState(true);
+  const [showSectionA, setShowSectionB] = useState(true);
   return (
     <div>
-      <h1>Header Text</h1>
-      <button onClick={() => setShowMessage(!showMessage)}>
-        {showMessage ? "Hide Message" : "Show Message"}
-      </button>
-      {showMessage && (<p>maybe?</p>)}
+      <div style={{ padding: "20px" }}>
+        <h1>Toggle a Specific Section</h1>
+
+        <button onClick={() => setShowSection((prev) => !prev)}>
+          {showSection ? "Hide Section" : "Show Section"}
+        </button>
+
+        {showSection && (
+          <div style={{ display: showSection ? "block" : "none" }}>
+            <p>This is the section you're hiding or showing 🫣</p>
+          </div>
+        )}
+      </div>
+
+      <div style={{ padding: "20px"}}>
+        <h1>Independent toggle</h1>
+
+        <button onClick={() => setShowSectionB((prev) => !prev)}>
+          {showSectionA ? "Hide Section" : "Show Section"}
+        </button>
+        
+        {showSectionA && (
+          <div>
+            <p>This is a seperate section</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
